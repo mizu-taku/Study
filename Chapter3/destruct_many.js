3f68ae644e9d01e4fe82c6eb2c2bd3893b4a3926
TakumuMizuhara <takumu0804.aaa@gmail.com>
2026-09-10 07:02:31 +0900

    演算子

---
+++ Chapter3/destruct_many.js
@@ -0,0 +1,12 @@
+//配列の左辺と右辺の個数の違いについて
+let data = [56, 40, 26, 82, 19, 17, 73, 99];
+let [x0, x1, x2, x3] = data;
+let [y0, y1, y2, y3, y4, y5, y6, y7, y8] = data;
+let [, , , , , , z6, z7] = data;
+
+console.log(x0);
+console.log(x3);
+console.log(y0);
+console.log(y8);
+console.log(z6);
+console.log(z7);
