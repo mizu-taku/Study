eec2926392a3e5d9a448fc34f4867ec9bcf5025f
TakumuMizuhara <mizuhara.takumu0804@gmail.com>
2026-09-14 06:30:05 +0900

    演算子

---
+++ Chapter3/equal_array.js
@@ -0,0 +1,4 @@
+//参照型の比較失敗例
+let data1 = ['Javascript', 'Ajax', 'ASP.NET'];
+let data2 = ['Javascript', 'Ajax', 'ASP.NET'];
+console.log(data1 == data2);
---
+++ Chapter3/equal_float.js
@@ -0,0 +1,5 @@
+const EPSIRON = 0.0001;
+let x = 0.98765;
+let y = 0.98764;
+
+console.log(Math.abs(x - y) < EPSIRON);
